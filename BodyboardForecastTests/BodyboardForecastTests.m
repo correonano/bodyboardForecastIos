6c9488508895e649c618226a046fab25f29fdc89
Ignacio Saslavsky <ignacio.saslavsky@rga.com>
2014-08-21 12:27:43 -0300

    Initial Commit

---
+++ BodyboardForecastTests/BodyboardForecastTests.m
@@ -0,0 +1,34 @@
+//
+//  BodyboardForecastTests.m
+//  BodyboardForecastTests
+//
+//  Created by Ignacio Saslavsky on 8/21/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BodyboardForecastTests : XCTestCase
+
+@end
+
+@implementation BodyboardForecastTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
